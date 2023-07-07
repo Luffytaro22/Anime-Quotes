834e251762a902fe5dbf5447d25483dba8a46d87
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-07 11:31:23 -0500

    Added an icon

--- src/components/input.js
+++ src/components/input.js
@@ -2,7 +2,10 @@ export default function Input() {
   return (
     <div id="input-container">
       <h1>Enter the anime!</h1>
-      <input id="input-quote" type="text" required />
+      <span id="span-container">
+        <input id="input-quote" type="text" required />
+        <i className="fa-solid fa-right-to-bracket" />
+      </span>
     </div>
   );
 }
