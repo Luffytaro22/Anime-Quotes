297e4b46b1a62909041768864afbf3924d528611
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-07 12:27:17 -0500

    Added a button with a listener to render a new quote of the same anime

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -5,12 +5,21 @@ export default function Quotes({ anime }) {
   const [quoteData, setQuote] = useState([]);
   const [hasError, setHasError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [newQuote, setNewQuote] = useState(false);
+
+  const anotherQuote = () => {
+    setNewQuote((prevState) => !prevState);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await fetch(`https://animechan.xyz/api/random/anime?title=${anime}`);
+        const response = await fetch(`https://animechan.xyz/api/random/anime?title=${anime}`, {
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        });
         const json = await response.json();
         setQuote(json);
       } catch (error) {
@@ -19,7 +28,7 @@ export default function Quotes({ anime }) {
       setIsLoading(false);
     };
     fetchData();
-  }, [anime]);
+  }, [anime, newQuote]);
 
   if (hasError) {
     return <div>Something went wrong!</div>;
@@ -33,6 +42,7 @@ export default function Quotes({ anime }) {
     <div id="Quote-container">
       <h2>{quoteData.quote}</h2>
       <p>{quoteData.character}</p>
+      <button type="button" onClick={anotherQuote}>Another One!</button>
     </div>
   );
 }
