f58c574a8dcb797f27aef9441add820a39557599
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-07-07 11:24:47 -0500

    Created component to render the input element

---
+++ src/components/input.js
@@ -0,0 +1,8 @@
+export default function Input() {
+  return (
+    <div id="input-container">
+      <h1>Enter the anime!</h1>
+      <input id="input-quote" type="text" required />
+    </div>
+  );
+}
